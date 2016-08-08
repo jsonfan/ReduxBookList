6a3dcd0dae0a623867ca33b9bf12a2e64b8f3ed1
Jason Fan <jsonfan@gmail.com>
2016-08-07 22:17:16 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# BookList on React-Redux
+
+A very simple React-Redux application using actions, components, containers and reducers.
+
+### Setup
+
+```
+$ npm install
+$ npm start
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101},
+    { title: 'Harry Potter', pages: 39},
+    { title: 'A Clash of Kings', pages: 85},
+    { title: 'Eloquent Ruby', pages: 1},
+    { title: 'OReilly Animal Book', pages: 201}
+  ]
+}
